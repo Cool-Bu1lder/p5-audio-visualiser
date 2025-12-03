72a001f213f8043eab1a2ca877418d728c9ff00c
Daniel Willoughby <omnical@outlook.com>
2025-12-03 18:29:46 +1000

    cleanup

--- src/sketch.js
+++ src/sketch.js
@@ -2,7 +2,7 @@ import p5 from 'p5'
 
 const sketch = (/** @type {p5} */ s) => {
   /** @type {p5.SoundFile} */ let sound
-  let fft
+  /** @type {p5.FFT} */ let fft
 
   s.preload = () => {
     sound = s.loadSound('/voice.mp3')
@@ -12,20 +12,10 @@ const sketch = (/** @type {p5} */ s) => {
     let cnv = s.createCanvas(100, 100)
     cnv.mouseClicked(togglePlay)
     fft = new p5.FFT()
-    sound.amp(0.2)
   }
 
   s.draw = () => {
     s.background(220)
-
-    let spectrum = fft.analyze()
-    s.noStroke()
-    s.fill(255, 0, 255)
-    for (let i = 0; i < spectrum.length; i++) {
-      let x = s.map(i, 0, spectrum.length, 0, s.width)
-      let h = -s.height + s.map(spectrum[i], 0, 255, s.height, 0)
-      s.rect(x, s.height, s.width / spectrum.length, h)
-    }
 
     let waveform = fft.waveform()
     s.noFill()
