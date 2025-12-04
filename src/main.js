421fbce3c4e44c44d06b1f5f119468c5ad59775b
Daniel Willoughby <omnical@outlook.com>
2025-12-04 11:38:56 +1000

    text to speech

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 // https://editor.p5js.org/jonfroehlich/sketches/d2euV09i
 // https://p5js.org/reference/p5.sound/p5.FFT/
+// https://editor.p5js.org/hao/sketches/8OhA3cn1J
+
+// https://github.com/espeak-ng/espeak-ng
+// https://github.com/abbr/text2wav.node.js
+// https://github.com/ianmarmour/espeak-ng.js
+// https://espeak.sourceforge.net/
+// https://huggingface.co/tasks/text-to-speech
+// https://strobotnik.com/unity/klattersynth/
+
+// https://github.com/xenova/phonemizer.js/
+// https://github.com/hexgrad/kokoro/tree/main/kokoro.js
 
 import './style.css'
 import p5 from 'p5'
--- src/sketch.js
+++ src/sketch.js
@@ -1,11 +1,38 @@
 import p5 from 'p5'
 
+//--------------------------------------------
+
+import { KokoroTTS } from 'kokoro-js'
+
+const model_id = 'onnx-community/Kokoro-82M-v1.0-ONNX'
+const tts = await KokoroTTS.from_pretrained(model_id, {
+  dtype: 'q8', // Options: "fp32", "fp16", "q8", "q4", "q4f16"
+  device: 'wasm', // Options: "wasm", "webgpu" (web) or "cpu" (node). If using "webgpu", we recommend using dtype="fp32".
+})
+
+const text =
+  "Life is like a box of chocolates. You never know what you're gonna get."
+const audio = await tts.generate(text, {
+  // Use `tts.list_voices()` to list all available voices
+  //voice: 'af_heart',
+  voice: 'am_santa',
+})
+
+console.log(tts.list_voices())
+
+//--------------------------------------------
+
 const sketch = (/** @type {p5} */ s) => {
   /** @type {p5.SoundFile} */ let sound
   /** @type {p5.FFT} */ let fft
 
   s.preload = () => {
-    sound = s.loadSound('/voice.mp3')
+    sound = new p5.SoundFile()
+    const rawChannelData = audio.audio
+    const channelDataArray = [rawChannelData]
+    const sampleRate = audio.sampling_rate
+    sound.setBuffer(channelDataArray)
+    sound.rate(sampleRate / sound.sampleRate())
   }
 
   s.setup = () => {
